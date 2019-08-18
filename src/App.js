994ce82065c4ae997cd50ddf00e2a658e29fb761
johnsontaiwo <josseytee@gmail.com>
2019-08-18 18:30:44 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+           
+          <h2>Job applications tracker</h2>
+        </div>
+        <p className="App-intro">
+     Keeping track of all jobs application I've submitted 
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+//img src={logo} className="App-logo" alt="logo" />
---
+++ src/Components/jobContainer.js
@@ -0,0 +1,1 @@
+jobContainer.js
